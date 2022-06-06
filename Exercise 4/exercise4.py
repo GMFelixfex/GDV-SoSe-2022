f3702bbadbc653fb6eed8c197f6d1be221472371
GMFelixfex <felix.poenitzsch@hs-furtwangen.de>
2022-06-07 00:53:41 +0200

    Changed

--- Exercise 4/exercise4.py
+++ Exercise 4/exercise4.py
@@ -2,7 +2,7 @@ from GDV_TrainingSet import Descriptor, TrainingSet
 import cv2
 import numpy as np
 
-
+# Best Match Function from Origonal File
 def findBestMatch(trainData, sample):
     # do the matching with FLANN
     FLANN_INDEX_KDTREE = 1
@@ -10,6 +10,7 @@ def findBestMatch(trainData, sample):
     search_params = dict(checks=50)
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(trainData.trainData, sample, k=1)
+
     # Sort by their distance.
     matches = sorted(matches, key=lambda x: x[0].distance)
     bestMatch = matches[0][0]
@@ -18,8 +19,9 @@ def findBestMatch(trainData, sample):
 
 click_x = 0
 click_y = 0
+
+# Click Function, to Select an Image to display
 def click(event, x, y, flags, param):
-    # grab references to the global variables
     global click_x
     global click_y
     global bigImage 
@@ -27,33 +29,30 @@ def click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         click_x = int(round(x,0)/16)
         click_y = int(round(y,0)/16)
-        print(str(click_x)+" "+str(click_y))
         bigImage = int(round((((click_y) + 50*(click_x))/2550)*2500,0))
 
-''' Define and compute or load the training data '''
-root_path = 'Exercise 4/data/101_ObjectCategories/'  # adjust this path if you have the files in some other folder
-# root_path = './data/temp/'  # you can use a smaller subset of the training data during development to save time
+
+# Setting up the Training Data
+root_path = 'Exercise 4/data/101_ObjectCategories/'  
 file_name = 'Exercise 4/data/data16.npz'
+
 trainData = TrainingSet(root_path)
-# either create and save the data
-# trainData.createTrainingData(Descriptor.TINY_COLOR16)
-# trainData.saveTrainingData(file_name)
-# or load the saved data if descriptor has not been changed.
-trainData.loadTrainingData(file_name)
 
+# Decide if you want to Train or Load the Data
+print("If you want to load instead of Training, press T")
+key = cv2.waitKey() & 0xFF
+if (key == ord("t")):
+    trainData.loadTrainingData(file_name)
+else:
+    trainData.createTrainingData(Descriptor.TINY_COLOR16)
+    trainData.saveTrainingData(file_name)
 
-
-
-
+# Loading the Image
 imgForMosaic =  cv2.imread('Exercise 4/bilder/cat2.png', cv2.IMREAD_COLOR)
 imgForMosaic =  cv2.resize(imgForMosaic,(800,800))
 
+# Dividing the Image in 2500 tiles
 imgArray = []
-# Note to remember: IMG[von-höhe:bis-höhe, von-breite:bis-breite]
-# for i in range(10):
-#    for k in range(10):
-#        imgArray.append(imgForMosaic[i*8:i*8+8,k*8:k*8+8])
-
 for i in range(50):
     for k in range(50):
         imgArray.append(imgForMosaic[i*16:i*16+16,k*16:k*16+16])
@@ -62,40 +61,43 @@ bestMatchedImages = []
 bestMatchedImagesFull = []
 bestMatchedImagesPart=[]
 
+
 assert(isinstance(trainData.descriptor, Descriptor))
 descr = trainData.descriptor
+
+# Matching a tile to a Picture
 for i in range(len(imgArray)):
     newcomer = np.ndarray(shape=(1, descr.getSize()),
                       buffer=np.float32(descr.compute(imgArray[i])),
                       dtype=np.float32)
     idx = findBestMatch(trainData, newcomer)
+
+    # Reading hte Picture
     matchedImage = cv2.imread(trainData.getFilenameFromIndex(idx), cv2.IMREAD_COLOR)
+
+    # Resizing and addihng the picture to 2 lists
     bestMatchedImages.append(cv2.resize(matchedImage,(16,16)))
     bestMatchedImagesPart.append(matchedImage)
     if(i%50 == 0 and i > 0):
+        # Creating a 2d List
         bestMatchedImagesFull.append(bestMatchedImagesPart)
         bestMatchedImagesPart = []
     
-    print("Bild Teil: "+str(i))
-bestMatchedImagesFull.append(bestMatchedImagesPart)
+    print("Picture Tile: "+str(i))
+# bestMatchedImagesFull.append(bestMatchedImagesPart)
+
 
 newCompletedImage = imgForMosaic.copy()
 j = 0
-#for i in range(10):
-#    for k in range(10):
-#        newCompletedImage[i*8:i*8+8,k*8:k*8+8] = bestMatchedImages[j]
-#        j = j+1
 
+# Replacing the Tiles with the Pictures
 for i in range(50):
     for k in range(50):
         newCompletedImage[i*16:i*16+16,k*16:k*16+16] = bestMatchedImages[j]
         j = j+1
 
-#for i in range(80):
-#    for k in range(80):
-#        newCompletedImage[i:i+1,k:k+1] = bestMatchedImages[j]
-#        j = j+1
 
+# Displaying the Images
 cv2.namedWindow("Original Image",cv2.WINDOW_GUI_NORMAL)
 cv2.namedWindow("Finished Image",cv2.WINDOW_GUI_NORMAL)
 cv2.namedWindow("Possible Image",cv2.WINDOW_GUI_NORMAL)
@@ -103,22 +105,20 @@ cv2.imshow("Original Image",imgForMosaic)
 cv2.imshow("Finished Image",newCompletedImage)
 cv2.setMouseCallback('Finished Image', click)
 
-bigImage = 1200
+bigImage = 0
 
+# Loop for Image selection
 while True:
-    if(click_x <= 49 and click_y <= 49):
-        cv2.imshow("Possible Image",bestMatchedImagesFull[click_y][click_x-1])
-
     key = cv2.waitKey(1) & 0xFF
-    
-    if (key == ord("w")):
-        bigImage = bigImage+1
-        print("Selected image: "+str(bigImage))
-    if (key == ord("s")):
-        bigImage = bigImage-1
-        print("Selected image: "+str(bigImage))
+
+    # Quitting
     if (key == ord("q")):
         break
+
+    # Save Function
     if (key == ord("t")):
         cv2.imwrite("Exercise 4/bilder/Mosaik.png",newCompletedImage)
 
+    # Displaying the Clicked Image
+    if(click_x <= 49 and click_y <= 49):
+        cv2.imshow("Possible Image",bestMatchedImagesFull[click_y][click_x-1])
